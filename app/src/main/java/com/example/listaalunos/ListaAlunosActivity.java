e30b74485ffaeecd3a67fa144a9af26129828c2e
Robson Ferreira <49348939+robsondmf1234@users.noreply.github.com>
2019-09-18 20:20:22 -0300

    First Commit

---
+++ app/src/main/java/com/example/listaalunos/ListaAlunosActivity.java
@@ -0,0 +1,25 @@
+package com.example.listaalunos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_alunos);
+
+        //Lista de Alunos
+        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
+        //Armazenando a referencia da instancia da lista de alunos
+        ListView listaAlunos = (findViewById(R.id.lista_alunos));
+        //ArrayAdpter , responsavel por converter as String da lista em Views,para ser armazenado no ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        //Setando o arrayadpter na ListView
+        listaAlunos.setAdapter(adapter);
+    }
+}
