54ad9538b05c11543fa11f90867224782b6ecad9
Robson Ferreira <49348939+robsondmf1234@users.noreply.github.com>
2019-10-13 22:06:43 -0300

    Atualizações: -Implementado a função de editar Alunos -Mostrando o nome do Aluno clicado

--- app/src/main/java/com/example/listaalunos/ListaAlunosActivity.java
+++ app/src/main/java/com/example/listaalunos/ListaAlunosActivity.java
@@ -28,6 +28,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         listaAlunos = (ListView) findViewById(R.id.lista_alunos);
 
+        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> lista, View item, int position, long id) {
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(position);
+                Toast.makeText(ListaAlunosActivity.this,"Aluno "+aluno.getNome()+" clicado!",Toast.LENGTH_LONG).show();
+            }
+        });
+
         Button novoAluno = (Button) findViewById(R.id.novo_aluno);
         novoAluno.setOnClickListener(new View.OnClickListener() {
             @Override
