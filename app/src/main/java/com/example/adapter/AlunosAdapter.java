456dc147705eacc15ca8e39179fce378b78b4cd6
Robson Ferreira <49348939+robsondmf1234@users.noreply.github.com>
2020-03-04 21:14:14 -0300

    Atualizações: Implementado a mudança de layout - OK

--- app/src/main/java/com/example/adapter/AlunosAdapter.java
+++ app/src/main/java/com/example/adapter/AlunosAdapter.java
@@ -1,11 +1,16 @@
 package com.example.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.listaalunos.R;
 import com.example.modelo.Aluno;
 
 import java.util.List;
@@ -39,9 +44,30 @@ public class AlunosAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView view = new TextView(context);
         Aluno aluno = alunos.get(position);
-        view.setText(aluno.toString());
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = convertView;
+        if (view == null) {
+            //inflando o xml para uma view
+            view = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView campoNome = view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = view.findViewById(R.id.item_foto);
+        String caminhoFoto = aluno.getCaminhoFoto();
+        if (caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapreduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            campoFoto.setImageBitmap(bitmapreduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
 
         return view;
     }
